9504ee60725c7ef4f6da1e4a425823620281cdac
chris-base <baseappsdev@gmail.com>
2022-09-09 13:30:49 -0500

    added slider/updating slider onchange

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("passwordLengthText").innerHTML = document.getElementById("lengthSlider").value;
+
+let sliderChanged = () => {
+  document.getElementById("passwordLengthText").innerHTML = document.getElementById("lengthSlider").value;
+};
